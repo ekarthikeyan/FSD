6f92f6f6b1578de74e0987a0d3f25450b0043750
ekarthikeyan <37636943+ekarthikeyan@users.noreply.github.com>
2018-06-27 21:41:14 -0500

    summary
    Changes

---
+++ day1.js
@@ -0,0 +1,11 @@
+console.log('hello world');
+function revStr(str) {
+    var revString = "";
+ 
+    for (var i = str.length - 1; i >= 0; i--) { 
+        revString += str[i]; 
+    }
+    return revString; 
+}
+ 
+console.log(revStr('abcd'));
