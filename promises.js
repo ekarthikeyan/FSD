fb88ab9c48b934ffad79b802feeb8d3888aa0bed
ekarthikeyan <37636943+ekarthikeyan@users.noreply.github.com>
2018-07-04 12:46:02 -0500

    new

---
+++ promises.js
@@ -0,0 +1,31 @@
+function asynchFn(callback){
+    console.log(callback)
+    setTimeout(function(){
+        callback("success from asynch")
+    }, 2000)
+}
+
+
+asynchFn(function(result){
+    console.log(result)
+})
+
+//Promises example
+
+function asynchFn(){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            // resolve("success from asynch")
+            reject("failure in asynch function")
+        }, 2000)
+    }) 
+}
+
+
+asynchFn()
+.then(function(result){
+    console.log(result)
+})
+.catch(function(error){
+    console.log(error)
+})
